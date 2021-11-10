1fafd3cdb53cc135915dfbe9dc80c4b3a8973486
terekwong <terekwong1@gmail.com>
2021-11-10 17:21:10 -0500

    Mobile App Project

---
+++ App project/ProjectDraft/app/src/main/java/com/example/projectdraft/Activity3.java
@@ -0,0 +1,31 @@
+package com.example.projectdraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Activity3 extends AppCompatActivity {
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
